804f8f0a2d4f9f28624837ed722620bd169aee9f
stea1th <stea1th@mail.ru>
2018-10-31 22:07:59 +0100

    kind delete()

--- src/main/java/art/school/web/kind/KindRestController.java
+++ src/main/java/art/school/web/kind/KindRestController.java
@@ -1,10 +1,10 @@
 package art.school.web.kind;
 
 import art.school.to.KindTo;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -22,5 +22,12 @@ public class KindRestController extends AbstractKindController{
                 i.getName(), i.getAdresse(), i.isAktiv(),
                 i.getRegistriert().truncatedTo(ChronoUnit.SECONDS).toString().replace("T", " "))));
         return list;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id){
+        System.out.println(id);
+        super.delete(id);
     }
 }
--- src/main/webapp/resources/js/kind.js
+++ src/main/webapp/resources/js/kind.js
@@ -1,7 +1,8 @@
 var ajaxUrl = "kind";
+var datatable;
 
 $(function () {
-    $('#kids').DataTable({
+    datatable = $('#kids').DataTable({
         "ajax": {
             "url": ajaxUrl,
             "dataSrc": ""
@@ -17,7 +18,15 @@ $(function () {
             {"data": "id"},
             {"data": "name"},
             {"data": "adresse"},
-            {"data": "aktiv"},
+            {"data": "aktiv",
+                "render": function (data, type, row) {
+                    if (type === "display") {
+                        return "<input type='checkbox' " + (data ? "checked" : "") + " onclick='#' style=''/>";
+                    }
+                    return data;
+                },
+                "className": "dt-body-center"
+            },
             {"data": "registriert"},
             {
                 "orderable": false,
@@ -36,15 +45,36 @@ $(function () {
 
 function renderEditBtn(data, type, row) {
     if (type === "display") {
-        return "<a onclick='#'><i class='fa fa-pencil' aria-hidden='true'></i></a>";
+        return "<a onclick='#'><i class='far fa-edit'></i></a>";
     }
 }
 
 function renderDeleteBtn(data, type, row) {
     if (type === "display") {
-        return "<a onclick='#'><i class='fa fa-pencil' aria-hidden='true'></i></a>";
+        return "<a onclick='deleteRow(" + row.id + ");'><i class='far fa-calendar-times'></i></a>";
     }
 }
+
+function deleteRow(id){
+    $.ajax({
+        url: ajaxUrl +"/"+ id,
+        type: "DELETE"
+    }).done(function(){
+        datatable.ajax.reload();
+    });
+}
+
+function updateTable() {
+    $.get(ajaxUrl, function(newDataArray){
+        datatable.clear();
+        datatable.rows.add(newDataArray);
+        datatable.draw();
+    });
+}
+
+
+
+
 
 
 
